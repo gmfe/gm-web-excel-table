1454a20aad1cdd2490c458e8ce9031d5ffe40d2d
kun <kunsam624@icloud.com>
2019-04-25 15:15:43 +0800

    remove some unuse pkg

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
-import "antd/dist/antd.less";
-import * as React from 'react';
-import * as ReactDOM from "react-dom";
-import { OrderTable1 } from './components/ordertable1/ordertable1';
-
 export * from './components'
-
 // 看看业务需要什么常量
 
 
-
-var mountNode = document.getElementById("app");
-ReactDOM.render(<OrderTable1 />, mountNode);
+// import "antd/dist/antd.less";
+// import * as React from 'react';
+// import * as ReactDOM from "react-dom";
+// import { OrderTable1 } from './components/ordertable1/ordertable1';
+// var mountNode = document.getElementById("app");
+// ReactDOM.render(<OrderTable1 />, mountNode);
